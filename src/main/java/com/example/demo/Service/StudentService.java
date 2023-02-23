4de3f2a8e829b802bf9dccefeacc6dfd4e6ad264
ATCHAYA-HUB <72772euit017@skcet.ac.in>
2023-02-23 09:41:48 +0530

    first commit

---
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.StudentEntity;
+import com.example.demo.Repository.StudentRepo;
+@Service
+public class StudentService {
+	@Autowired
+	StudentRepo stRepo;
+	public StudentEntity saveDetails(StudentEntity e) {
+		return stRepo.save(e);
+	}
+
+}
